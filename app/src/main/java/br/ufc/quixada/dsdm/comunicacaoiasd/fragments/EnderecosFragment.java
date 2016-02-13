cad62963f238c8ca0458b3a2a3e688c69e445597
italopessoa <italoneypessoa@gmail.com>
2016-02-13 16:33:40 -0300

    configuração para suportar google maps

--- app/src/main/java/br/ufc/quixada/dsdm/comunicacaoiasd/fragments/EnderecosFragment.java
+++ app/src/main/java/br/ufc/quixada/dsdm/comunicacaoiasd/fragments/EnderecosFragment.java
@@ -1,11 +1,21 @@
 package br.ufc.quixada.dsdm.comunicacaoiasd.fragments;
 
 
+import android.support.v4.app.FragmentManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import br.ufc.quixada.dsdm.comunicacaoiasd.R;
 
@@ -15,11 +25,36 @@ import br.ufc.quixada.dsdm.comunicacaoiasd.R;
  */
 public class EnderecosFragment extends Fragment {
 
+    private SupportMapFragment map;
+    private RelativeLayout fragment;
 
-    public EnderecosFragment() {
-        // Required empty public constructor
+    public EnderecosFragment()
+    {
+
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentManager fm = getChildFragmentManager();
+        map = (SupportMapFragment) fm.findFragmentById(R.id.map);
+
+        ((SupportMapFragment) fm.findFragmentById(R.id.map)).getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                googleMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(0, 0))
+                        .title("Hello world"));
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
